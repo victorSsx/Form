c3c1eeca43c5201a818617898b6343b3fabfd295
victorx10 <vs469963@gmail.com>
2025-08-24 23:40:53 -0300

    Deploy Flask com minha estrutura no Vercel

---
+++ projeto/api/index.py
@@ -0,0 +1,10 @@
+import sys # Para manipulação do sistema
+import os # Para manipulação de caminhos
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) # Adiciona o diretório pai ao sys.path
+
+from app import app # Importa a aplicação Flask do arquivo app.py
+
+# handler que a Vercel precisa
+def handler(environ, start_response):
+    return app(environ, start_response) # Chama a aplicação Flask
